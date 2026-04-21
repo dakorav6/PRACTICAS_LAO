27f43a1ef44a7a2fbbaa383e4fb62e0b2fa64fbd
Dakora <88762464+dakorav6@users.noreply.github.com>
2026-04-21 19:33:58 +0000

    primerAngular

--- JS/Angular/primerAngular/src/app/components/login/login.spec.ts
+++ JS/Angular/primerAngular/src/app/components/login/login.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Navbar } from './navbar';
+import { Login } from './login';
 
-describe('Navbar', () => {
-  let component: Navbar;
-  let fixture: ComponentFixture<Navbar>;
+describe('Login', () => {
+  let component: Login;
+  let fixture: ComponentFixture<Login>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Navbar],
+      imports: [Login],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Navbar);
+    fixture = TestBed.createComponent(Login);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
