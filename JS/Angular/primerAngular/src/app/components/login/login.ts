f78e111975c4d3c30f544129e0c8f82b85c22b16
Dakora <88762464+dakorav6@users.noreply.github.com>
2026-05-21 23:22:03 +0000

    modal

--- JS/Angular/primerAngular/src/app/components/login/login.ts
+++ JS/Angular/primerAngular/src/app/components/login/login.ts
@@ -7,12 +7,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+
 
 
 import { Component, inject, signal } from '@angular/core'; // Añadimos inject aquí
 import { Router } from '@angular/router'; 
-
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -26,16 +26,14 @@ import { Router } from '@angular/router';
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
-    RouterLink
   ]
 })
 export class Login {
 // AQUÍ, dentro de la clase, es donde usas inject()
   
+private miNavegacion= inject(Router);
+private cerrarNav=inject(MatDialogRef);
 
-validarCuenta(){}
-
-miNavegacion: Router = inject(Router);
 
 usuario: string= ``;
 contrasena: string= ``;
@@ -43,10 +41,9 @@ contrasena: string= ``;
 
 entrar():void {
 
-  
-if(this.usuario==`produ02` && this.contrasena==`erat1027`)  
-this.miNavegacion.navigate(['/principal']);
-
-}
-
+    if(this.usuario==`produ02` && this.contrasena==`erat1027`) {
+        this.cerrarNav.close();  // 1. cierra el modal
+        this.miNavegacion.navigate(['/principal']);
+      }
+  }
 }
